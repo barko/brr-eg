644c1d972879320e6c679ed2714ba2a246c11304
barko <mika@illouz.net>
2019-04-10 00:28:30 -0700

    handling malformed requests

--- calc/back/server.ml
+++ calc/back/server.ml
@@ -35,26 +35,36 @@ let server () =
 
     let%lwt (status, body, headers) =
       match Request.meth req with
-      | `POST ->
-        let%lwt body_s = Cohttp_lwt.Body.to_string body in
-        let f2b = P.front_to_back_of_string body_s in
-        let%lwt state = Lwt_mvar.take state_mvar in
-        let state, response =
-          match f2b with
-          | `NewSession ->
-            let session = Int64.to_string (Random.int64 Int64.max_int) in
-            let state = SS.add session state in
-            state, `NewSession session
+      | `POST -> (
+          let%lwt body_s = Cohttp_lwt.Body.to_string body in
+          try
+            let f2b = P.front_to_back_of_string body_s in
+            let%lwt state = Lwt_mvar.take state_mvar in
+            let state, response =
+              match f2b with
+              | `NewSession ->
+                let session = Int64.to_string (Random.int64 Int64.max_int) in
+                let state = SS.add session state in
+                state, `NewSession session
 
-          | `Add (session, (a, b)) ->
-            match SS.find_opt session state with
-            | None -> state, `InvalidSession
-            | Some _ -> state, `Add (a + b)
-        in
-        let%lwt () = Lwt_mvar.put state_mvar state in
-        let b2f = P.string_of_back_to_front response in
-        let headers = add_access_control req headers in
-        Lwt.return (`OK, b2f, headers)
+              | `Add (session, (a, b)) ->
+                match SS.find_opt session state with
+                | None -> state, `InvalidSession
+                | Some _ -> state, `Add (a + b)
+            in
+            let%lwt () = Lwt_mvar.put state_mvar state in
+            let b2f = P.string_of_back_to_front response in
+            let headers = add_access_control req headers in
+            Lwt.return (`OK, b2f, headers)
+
+          with
+          | Yojson.Json_error err_msg
+          | Atdgen_runtime__Oj_run.Error err_msg ->
+            let response_headers = Header.add headers
+                "content-type" "text/plain" in
+            let body = "Error: " ^ err_msg in
+            Lwt.return (`OK, body, response_headers)
+        )
 
       | _ ->
         Lwt.return not_found
@@ -66,13 +76,7 @@ let server () =
   Server.create ~mode (Server.make ~callback ())
 
 let _ =
-  try
-    (* see https://github.com/mirage/ocaml-cohttp/issues/511 *)
-    Lwt.async_exception_hook := ignore;
-    ignore (Lwt_main.run (server ()))
-  with
-  | Yojson.Json_error err_msg
-  | Atdgen_runtime__Oj_run.Error err_msg ->
-    print_endline err_msg;
-    exit 1
+  (* see https://github.com/mirage/ocaml-cohttp/issues/511 *)
+  Lwt.async_exception_hook := ignore;
+  ignore (Lwt_main.run (server ()))
 
